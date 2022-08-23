6a4c2e1ce580966bfabae0b30223514f00bd6452
gu-does-git <gu.does.dev@gmail.come>
2022-08-22 22:52:16 -0300

    commit to trigger vercel redeployment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import '../styles/main.css'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps  }: AppProps) {
   return (
     <>
       <Head>
