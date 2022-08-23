4beccbca3eaa12545c77b656f8af5edc728c7787
gu-does-git <gu.does.dev@gmail.come>
2022-08-22 23:01:16 -0300

    changing meta stuff

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,9 +16,9 @@ class MyDocument extends Document {
   render() {
     const meta = {
       title: 'gudoes.dev - Gustavo Rocha',
-      description: 'My website 😉',
+      description: 'Trust me 😉',
       image:
-        'https://images.unsplash.com/photo-1614583224978-f05ce51ef5fa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1572&q=80',
+        'https://avatars.githubusercontent.com/u/42705462?v=4',
     }
 
     return (
