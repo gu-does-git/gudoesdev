6e366aade0522600ddc145920698aef8b96d95bb
gu-does-git <gu.does.dev@gmail.come>
2022-11-14 20:42:28 -0300

    initial usage of astro and astro-paper

---
+++ src/config.ts
@@ -0,0 +1,116 @@
+import type { SocialsObject } from "./types";
+
+export const SITE = {
+  website: "https://www.gudoes.dev/",
+  author: "Gustavo Rocha",
+  desc: "My first blog!",
+  title: "gudoesdev",
+  ogImage: "default-og.png",
+  lightAndDarkMode: true,
+  postPerPage: 3,
+};
+
+export const LOGO_IMAGE = {
+  enable: false,
+  svg: true,
+  width: 216,
+  height: 46,
+};
+
+export const SOCIALS: SocialsObject = [
+  {
+    name: "Github",
+    href: "https://github.com/gu-does-git",
+    active: true,
+  },
+  {
+    name: "Facebook",
+    href: "https://github.com/satnaing/astro-paper",
+    active: false,
+  },
+  {
+    name: "Instagram",
+    href: "https://github.com/satnaing/astro-paper",
+    active: false,
+  },
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/gustavo-rocha326",
+    active: true,
+  },
+  {
+    name: "Mail",
+    href: "mailto:gu.does.dev@gmail.com",
+    active: true,
+  },
+  {
+    name: "Twitter",
+    href: "https://github.com/satnaing/astro-paper",
+    active: false,
+  },
+  {
+    name: "Twitch",
+    href: "https://github.com/satnaing/astro-paper",
+    active: false,
+  },
+  {
+    name: "YouTube",
+    href: "https://github.com/satnaing/astro-paper",
+    active: false,
+  },
+  {
+    name: "WhatsApp",
+    href: "https://github.com/satnaing/astro-paper",
+    active: false,
+  },
+  {
+    name: "Snapchat",
+    href: "https://github.com/satnaing/astro-paper",
+    active: false,
+  },
+  {
+    name: "Pinterest",
+    href: "https://github.com/satnaing/astro-paper",
+    active: false,
+  },
+  {
+    name: "TikTok",
+    href: "https://github.com/satnaing/astro-paper",
+    active: false,
+  },
+  {
+    name: "CodePen",
+    href: "https://github.com/satnaing/astro-paper",
+    active: false,
+  },
+  {
+    name: "Discord",
+    href: "https://github.com/satnaing/astro-paper",
+    active: false,
+  },
+  {
+    name: "GitLab",
+    href: "https://github.com/satnaing/astro-paper",
+    active: false,
+  },
+  {
+    name: "Reddit",
+    href: "https://github.com/satnaing/astro-paper",
+    active: false,
+  },
+  {
+    name: "Skype",
+    href: "https://github.com/satnaing/astro-paper",
+    active: false,
+  },
+  {
+    name: "Steam",
+    href: "https://github.com/satnaing/astro-paper",
+    active: false,
+  },
+  {
+    name: "Telegram",
+    href: "https://github.com/satnaing/astro-paper",
+    active: false,
+  },
+];
