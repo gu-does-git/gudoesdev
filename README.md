4030cd7cc7baf05e75d28726598563293adf86f8
gu-does-git <gu.does.dev@gmail.come>
2022-11-14 19:50:49 -0300

    cleaning branch

--- README.md
+++
@@ -1,5 +0,0 @@
-## NextJS v1
-
-This is a new portfolio/blog/snippet repo/repo lister that i'm building on my free time to get some production experience on using NextJS. 
-
-*based on this [Starter Demo](https://demo.vercel.blog).
--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const withNextra = require("nextra")({
-  theme: "nextra-theme-blog",
-  themeConfig: "./theme.config.js",
-});
-
-const nextConfig = {};
-
-module.exports = withNextra(nextConfig);
