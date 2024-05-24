e39b4bcbbfae19217fbc0adadd91c810ec456e4a
gu-does-git <gu.does.dev@gmail.com>
2024-05-23 22:48:51 -0300

    feat: created collections and i18n support structure

---
+++ src/content/config.ts
@@ -0,0 +1,25 @@
+// Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+  // Export a single `collections` object to register your collection(s)
+  export const collections = {
+  // Define a `type` and `schema` for each collection
+  posts: defineCollection({
+    type: 'content',
+    schema: z.object({
+      author: z.string().default('Gustavo Rocha'),
+      pubDatetime: z.date(),
+      modDatetime: z.date().optional().nullable(),
+      title: z.string(),
+      featured: z.boolean().optional(),
+      draft: z.boolean().optional(),
+      tags: z.array(z.string()).default(["others"]),
+      image: z.string(),
+      description: z.string(),
+      canonicalURL: z.string().optional(),
+      // image: z.object({
+        //   url: z.string(),
+        //   alt: z.string()
+        // }),
+      })
+    }),
+};
