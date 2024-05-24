ce4892c1794cd2243a882d9811ceca12cff21c97
gu-does-git <gu.does.dev@gmail.com>
2024-05-23 23:16:25 -0300

    feat: added new collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,12 +1,12 @@
 // Import utilities from `astro:content`
 import { z, defineCollection } from "astro:content";
-  // Export a single `collections` object to register your collection(s)
-  export const collections = {
+// Export a single `collections` object to register your collection(s)
+export const collections = {
   // Define a `type` and `schema` for each collection
   posts: defineCollection({
-    type: 'content',
+    type: "content",
     schema: z.object({
-      author: z.string().default('Gustavo Rocha'),
+      author: z.string().default("Gustavo Rocha"),
       pubDatetime: z.date(),
       modDatetime: z.date().optional().nullable(),
       title: z.string(),
@@ -16,6 +16,17 @@ import { z, defineCollection } from "astro:content";
       image: z.string(),
       description: z.string(),
       canonicalURL: z.string().optional(),
-      })
     }),
+  }),
+  projects: defineCollection({
+    type: "content",
+    schema: z.object({
+      name: z.string(),
+      description: z.string(),
+      url: z.string(),
+      image: z.string().optional(),
+      tags: z.array(z.string()).optional(),
+      featured: z.boolean().optional(),
+    }),
+  }),
 };
