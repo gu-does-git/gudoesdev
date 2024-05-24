8406ed18a84edc6c2ddf29a17d7dbf14edf9a40a
gu-does-git <gu.does.dev@gmail.com>
2024-05-23 23:50:01 -0300

    feat: added astro-icon/astro-themes and added header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), icon()],
   i18n: {
     defaultLocale: "en",
-    locales: ["en", "pt-br"],
+    locales: ["en", "pt-br"]
   }
 });
